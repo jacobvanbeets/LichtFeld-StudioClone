424aa6c853164da4eb5a6a4ba3b8410f8dd12b00
Janusch Patas <janusch.patas@gmx.de>
2026-02-03 15:09:32 +0100

    Remove literal values from build_info stub to avoid dirty diff on every build

--- src/python/stubs/lichtfeld/build_info.pyi
+++ src/python/stubs/lichtfeld/build_info.pyi
@@ -1,15 +1,7 @@
-
-
-version: str = 'v0.4.2-3-g6a4c06d4-dirty'
-
-commit: str = '6a4c06d4'
-
-build_type: str = 'Release'
-
-platform: str = 'Linux'
-
-cuda_gl_interop: bool = True
-
-repo_url: str = 'https://github.com/MrNeRF/LichtFeld-Studio'
-
-website_url: str = 'https://lichtfeld.io'
+version: str
+commit: str
+build_type: str
+platform: str
+cuda_gl_interop: bool
+repo_url: str
+website_url: str
